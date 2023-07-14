1ce6e49aedba303faff48a26ce6d13d560ba73aa
Sammie <lythekiet333@gmail.com>
2023-07-14 21:06:20 +0930

    Drop-letters animation
    Added dropping letters animation for the intro

--- front-end/index.js
+++ front-end/index.js
@@ -4,6 +4,27 @@ window.addEventListener('scroll', function() {
     const fadeInElements = document.querySelectorAll('.fade-in-text');
     // Drop letters slowly
     const introElements = this.document.querySelectorAll('.intro');
+
+    introElements.forEach(function(element){
+        // Retrieve the viewport coordinates i.e (0,700)
+        const elementTop = element.getBoundingClientRect().top;
+        const elementBottom = element.getBoundingClientRect().bottom;
+        // If in the viewport then perform animation
+        if (elementTop < window.innerHeight && elementBottom > 0) {
+            const paragraph = element.querySelector('p');
+            //Split the texts into letters
+            const letters = paragraph.textContent.split('').map(letter => `<span class="letter">${letter}</span>`).join('');
+            // Make span element for each letter
+            paragraph.innerHTML = letters;
+      
+            const letterElements = paragraph.querySelectorAll('.letter');
+            letterElements.forEach(function(letter, index) {
+            // Add drop-letters animation for each letter to simulate dropping letters
+              letter.style.animation = `drop-letters 1.5s ease-in-out forwards ${index * 0.1}s`;
+            });
+            paragraph.classList.add('fade-in'); // Add the fade-in class
+          }
+    });
     // Loop through each fade-in element
     fadeInElements.forEach(function(element) {
       const elementTop = element.getBoundingClientRect().top;
@@ -17,6 +38,7 @@ window.addEventListener('scroll', function() {
       }
     });
 
+    
 
   });
   
