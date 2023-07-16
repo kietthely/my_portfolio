09a7bd3948c3efee977419aa6a27b0b9fb1cadb2
Sammie <lythekiet333@gmail.com>
2023-07-16 17:09:46 +0930

    Added dropLettersAnimations
    Added drop letters animation like LED animation in some shopping centres.

--- front-end/index.js
+++ front-end/index.js
@@ -2,7 +2,30 @@
 window.addEventListener('scroll', function() {
     fadeInAnimation();
 });
+function dropLettersAnimation(){
+    const introElements = this.document.querySelectorAll('.intro');
 
+    introElements.forEach(function(element){
+        // Retrieve the viewport coordinates i.e (0,700)
+        const elementTop = element.getBoundingClientRect().top;
+        const elementBottom = element.getBoundingClientRect().bottom;
+        // If in the viewport then perform animation
+        if (elementTop < window.innerHeight && elementBottom > 0) {
+            const paragraph = element.querySelector('p');
+            //Split the texts into letters
+            const letters = paragraph.textContent.split('').map(letter => `<span class="letter">${letter}</span>`).join('');
+            // Make span element for each letter
+            paragraph.innerHTML = letters;
+      
+            const letterElements = paragraph.querySelectorAll('.letter');
+            letterElements.forEach(function(letter, index) {
+            // Add drop-letters animation for each letter to simulate dropping letters
+              letter.style.animation = `drop-letters 1.5s ease-in-out forwards ${index * 0.1}s`;
+            });
+            paragraph.classList.add('fade-in'); // Add the fade-in class
+          }
+    });
+  }
 function fadeInAnimation(){
     // Make texts slowly appear
     const fadeInElements = document.querySelectorAll('.fade-in-text');
