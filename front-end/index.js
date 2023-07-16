5035b6c69f33741345bdc3508e591f84509ed981
Sammie <lythekiet333@gmail.com>
2023-07-16 18:13:17 +0930

    Update dropLettersAnimation
    Change name to LEDLettersAnimation for clearer meaning
    Add minor changes to the delay time to the animation

--- front-end/index.js
+++ front-end/index.js
@@ -2,7 +2,8 @@
 window.addEventListener('scroll', function() {
     fadeInAnimation();
 });
-function dropLettersAnimation(){
+window.addEventListener('load', function(){LEDLettersAnimation();});
+function LEDLettersAnimation(){
     const introElements = this.document.querySelectorAll('.intro');
 
     introElements.forEach(function(element){
@@ -20,7 +21,7 @@ function dropLettersAnimation(){
             const letterElements = paragraph.querySelectorAll('.letter');
             letterElements.forEach(function(letter, index) {
             // Add drop-letters animation for each letter to simulate dropping letters
-              letter.style.animation = `drop-letters 1.5s ease-in-out forwards ${index * 0.1}s`;
+              letter.style.animation = `drop-letters 1.7s infinite ease-in-out forwards ${index * 0.1}s`;
             });
             paragraph.classList.add('fade-in'); // Add the fade-in class
           }
